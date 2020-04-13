667633eb2e3f01429ff31362553055bc2674598a
alienzj <alienchuj@gmail.com>
2020-04-13 13:55:46 +0800

    update snakehelper

--- shell/fish/snakehelper.fish
+++ shell/fish/snakehelper.fish
@@ -3,6 +3,7 @@
 function snakehelper --wraps snakemake --description 'hello, snakemake'
     echo "snakemake"
     echo "snakeparse"
+    echo "snakeinit <begin> <assembler> <samples.tsv>"
     echo "snakelist <configfile>"
     echo "snakedebug <configfile> <target>"
     echo "snakerun <configfile> <target> <threads>"
